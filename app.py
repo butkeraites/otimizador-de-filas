6d2083ca9a569e7228e1fb209ed3bc4903faa420
Renan Brito Butkeraites <rbritobut?@gmail.com>
2021-02-05 01:22:03 -0300

    Adicao da funcao de otimizacao de distancias.

--- app.py
+++ app.py
@@ -2,15 +2,20 @@ import os
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 
-from otimizador_filas import otimizar_filas
+from otimizador_filas import otimizar_filas, otimizar_distancia
 
 app = Flask(__name__)
 api = Api(app)
 
-class OtimizarFilas(Resource):
-    @staticmethod
-    def post():
-        posted_data = request.get_json()
+def maximizar_alunos(posted_data):
+    if  ('largura_sala' in posted_data) and \
+        ('comprimento_sala' in posted_data) and \
+        ('largura_cadeira' in posted_data) and \
+        ('comprimento_cadeira' in posted_data) and \
+        ('num_carteiras' in posted_data) and \
+        ('largura_corredores' in posted_data) and \
+        ('distancia_minima' in posted_data):
+        
         largura_sala = posted_data['largura_sala']
         comprimento_sala = posted_data['comprimento_sala']
         largura_cadeira = posted_data['largura_cadeira']
@@ -20,12 +25,53 @@ class OtimizarFilas(Resource):
         distancia_minima = posted_data['distancia_minima']
 
         return jsonify(otimizar_filas(largura_sala,
-                                      comprimento_sala,
-                                      largura_cadeira,
-                                      comprimento_cadeira,
-                                      num_carteiras,
-                                      largura_corredores,
-                                      distancia_minima))
+                                    comprimento_sala,
+                                    largura_cadeira,
+                                    comprimento_cadeira,
+                                    num_carteiras,
+                                    largura_corredores,
+                                    distancia_minima))
+    else:
+        return jsonify({'erro' : 'Chave nao encontrada'})
+
+def maximizar_distancia(posted_data):
+    if  ('largura_sala' in posted_data) and \
+        ('comprimento_sala' in posted_data) and \
+        ('largura_cadeira' in posted_data) and \
+        ('comprimento_cadeira' in posted_data) and \
+        ('num_carteiras' in posted_data) and \
+        ('largura_corredores' in posted_data) and \
+        ('numero_de_alunos' in posted_data):
+        
+        largura_sala = posted_data['largura_sala']
+        comprimento_sala = posted_data['comprimento_sala']
+        largura_cadeira = posted_data['largura_cadeira']
+        comprimento_cadeira = posted_data['comprimento_cadeira']
+        num_carteiras = posted_data['num_carteiras']
+        largura_corredores = posted_data['largura_corredores']
+        numero_de_alunos = posted_data['numero_de_alunos']
+
+        return jsonify(otimizar_distancia(largura_sala,
+                                    comprimento_sala,
+                                    largura_cadeira,
+                                    comprimento_cadeira,
+                                    num_carteiras,
+                                    largura_corredores,
+                                    numero_de_alunos))
+    else:
+        return jsonify({'erro' : 'Chave nao encontrada'})
+class OtimizarFilas(Resource):
+    @staticmethod
+    def post():
+        posted_data = request.get_json()
+        if posted_data:
+            if ('modelo' in posted_data):
+                if (posted_data['modelo'] == 'max_alunos'):
+                    return maximizar_alunos(posted_data)
+                else:
+                    return maximizar_distancia(posted_data)
+        else:
+            return jsonify({'erro' : 'Mensagem vazia'})
 
 api.add_resource(OtimizarFilas, '/otimizar-filas')
 
--- otimizador_filas.py
+++ otimizador_filas.py
@@ -9,7 +9,7 @@ def otimizar_filas(hs,
                    hc,
                    vc,
                    m,
-                   n, #LISTA COM ARGURAS DOS CORREDORES VERTICAIS
+                   n, #LISTA COM LARGURAS DOS CORREDORES VERTICAIS
                    dmin):
   #PAINEL DE CONTROLE
   #hs = 10 #largura da sala
@@ -123,6 +123,130 @@ def otimizar_filas(hs,
   #print('Problema resolvido em %d nos do branch-and-bound' % solver.nodes())
   # [END advanced]
 
+def otimizar_distancia(hs,
+                        vs,
+                        hc,
+                        vc,
+                        m,
+                        n, #LISTA COM LARGURAS DOS CORREDORES VERTICAIS
+                        num_alunos):
+  #PAINEL DE CONTROLE
+  #hs = 10 #largura da sala
+  #vs = 7 #comprimento da sala
+
+  #hc = 0.5 #largura da cadeira
+  #vc = 0.5 #comprimento da cadeira
+
+  vr = (vs-m*vc)/(m) #comprimento do corredor horizontal
+
+  #dmin = 1 #ditancia minima entre pessoas
+
+  #CALCULO DOS PARAMETROS
+  cadeiras = []
+  coordenada_y = 0
+  for j in range(m):
+    if j > 0: 
+      coordenada_y += vc + vr
+    coordenada_x = 0
+    for i in (range(len(n)+1)):
+      if i > 0:
+        coordenada_x = coordenada_x + hc + n[i-1]
+      cadeira = {
+          'id' : j*(len(n)+1)+i,
+          'fileira' : i+1,
+          'cadeira' : j+1,
+          'x' : coordenada_x,   #coordenadas consideradas do canto superior
+          'y' : coordenada_y    #esquerdo do retangulo de ocupacao do aluno
+      }
+      cadeiras.append(cadeira)
+  #print(pd.DataFrame(cadeiras))
+  
+  M = 0.0
+
+  distancias = {}
+  for cadeira_l in cadeiras:
+    for cadeira_k in cadeiras:
+      if cadeira_l['id'] < cadeira_k['id']:
+        distancia = {
+            'id_cadeira_1' : cadeira_l['id'],
+            'id_cadeira_k' : cadeira_k['id'],
+            'distancia' : math.sqrt((cadeira_l['x']-cadeira_k['x'])**2+
+                                    (cadeira_l['y']-cadeira_k['y'])**2)
+        }
+        if distancia['distancia'] > M:
+          M = distancia['distancia']
+        if cadeira_l['id'] in distancias:
+          distancias[cadeira_l['id']][cadeira_k['id']] = distancia['distancia']
+        else:
+          distancias[cadeira_l['id']] = {}
+          distancias[cadeira_l['id']][cadeira_k['id']] = distancia['distancia']
+    
+  #GARANTIA
+  M += 1
+
+  # [START solver]
+  # Create the mip solver with the CBC backend.
+  solver = pywraplp.Solver('otimizacao_distancias',
+                            pywraplp.Solver.CBC_MIXED_INTEGER_PROGRAMMING)
+  # [END solver]
+  # [START variables]
+  # x binary integer variable.
+  x = {}
+  for cadeira in cadeiras:
+    x[cadeira['id']] = solver.BoolVar('x[%i]' % cadeira['id'])
+
+  r = solver.NumVar(0.0, M, 'r')
+  print('Numero de variaveis =', solver.NumVariables())
+  # [END variables]
+  # [START constraints]
+  for cadeira_l in cadeiras:
+    for cadeira_k in cadeiras:
+      if cadeira_l['id'] < cadeira_k['id']:
+        solver.Add(M*(2-x[cadeira_l['id']] -x[cadeira_k['id']]) + distancias[cadeira_l['id']][cadeira_k['id']] - r >= 0)
+  solver.Add(solver.Sum(x[cadeira['id']] for cadeira in cadeiras) == num_alunos)
+  
+  print('Numero de restricoes =', solver.NumConstraints())
+  # [END constraints]
+  # [START objective]
+  # Maximize \sum\limits_{l\in L}x_l
+  solver.Maximize(r)
+  # [END objective]
+
+  # [START solve]
+  status = solver.Solve()
+  # [END solve]
+
+  # [START print_solution]
+  if status == pywraplp.Solver.OPTIMAL:
+      print('Solucao:')
+      print('Funcao objetivo =', solver.Objective().Value())
+      resposta = []
+      for l in range(len(x)):
+        resposta.append(x[l].solution_value())
+      resposta = np.reshape(resposta,(m,(len(n)+1)))
+      return {'status' : 1,
+              'distancia_minima' : solver.Objective().Value(),
+              'numero_alunos' : num_alunos,
+              'num_fileiras' : (len(n)+1),
+              'num_carteiras' : m,
+              "largura_corredores_verticais": n,
+ 	            "largura_corredor_horizontal": vr,
+              'resposta' : resposta.T.tolist(),
+              'tempo_resolucao' : solver.wall_time(),
+              'num_iteracoes' : solver.iterations(),
+              'num_nodes' : solver.nodes()}
+  else:
+      return {'status' : 0,
+              'resposta':'O problema nao tem solucao otima.'}
+  # [END print_solution]
+
+  # [START advanced]
+  #print('\nEstatistica de resolucao:')
+  #print('Problema resolvido em %f milisegundos' % solver.wall_time())
+  #print('Problema resolvido em %d iteracoes' % solver.iterations())
+  #print('Problema resolvido em %d nos do branch-and-bound' % solver.nodes())
+  # [END advanced]
+
 def main():
   hs = 10
   vs = 7
@@ -130,9 +254,10 @@ def main():
   vc = 0.5
   m = 6
   n = [1,2,1,1,2,1]
-  dmin = 1.0
-  print(otimizar_filas(hs,vs,hc,vc,m,n, dmin))
-
+  #dmin = 1.0
+  num_alunos = 20
+  #print(otimizar_filas(hs,vs,hc,vc,m,n, dmin))
+  print(otimizar_distancia(hs,vs,hc,vc,m,n, num_alunos))
   
 if __name__ == "__main__":
   main()
